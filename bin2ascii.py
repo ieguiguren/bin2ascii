f5f3ba9b307549c51a067b2f00e2b36488c2207b
XiR_ <donosor00@gmail.com>
2015-03-05 17:31:06 +0100

    parse input

--- bin2ascii.py
+++ bin2ascii.py
@@ -3,6 +3,7 @@
 mensaje = True
 while mensaje != '':
 	mensaje = raw_input('Binary sequence: ').replace(" ","")
+	mensaje = '0' * ( 8 - (len(mensaje) % 8)) + mensaje
 	uncoded = [ mensaje[byte:byte+8]  for byte in range(0, len(mensaje), 8) ]
 	chain = []
 	for i in uncoded:
